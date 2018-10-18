0f4283e0faf2e7f816093545fbace9d289825e62
Raphael Eicher <raphael.eicher@gmail.com>
2018-10-18 23:02:22 +0200

    correted some typos

--- jenkins_shell_code.sh
+++ jenkins_shell_code.sh
@@ -19,11 +19,12 @@ if [ $ERR -eq 0 ]; then
     IP=$(sudo docker inspect -f {{.NetworkSettings.IPAddress}} \
         jenkins_identidock_1)
     CODE=$( curl -sL -w "%{http_code}" $IP:9090/monster/bla \
-        -o /dev/ null) || true
+        -o /dev/null) || true
     if [ $CODE -ne 200 ]; then
         echo "Site returned " $CODE
         ERR=1
     fi
+fi
 
 #Pull down the system
 sudo docker-compose $COMPOSE_ARGS stop
